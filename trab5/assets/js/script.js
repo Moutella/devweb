faa348fcbe8d4f4300313aa9c7e45f7160cefd24
moutella <joaogabrielmsc@gmail.com>
2019-05-18 11:52:23 -0300

    Versão entregue

--- trab5/assets/js/script.js
+++ trab5/assets/js/script.js
@@ -1,50 +1,15 @@
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
    
-$(function () {
-   function validaEntradaFunction(){
-      let nome = document.querySelector("#nome");
-      if(nome.value == ''){
-         nome.classList.add("is-invalid");
-         return false;
-      }
-      return true;
-   }
-   $("#enviar").click(function() {
-      let nome = document.querySelector("#nome");
-         if(validaEntradaFunction()){
-            $("#listaEsq").append($("<li class='list-group-item'>" + nome.value + botaoMover() + "</li>").hide().fadeIn(1000));
-         }
-         nome.value = '';
-   })
-   $("#nome").on('input', function(){
-      if($(this).hasClass("is-invalid")){
-         nome.classList.remove("is-invalid");
-      }
-   })
-   function botaoMover(){
-              
-      return "<button type='button' name='mover' style='float: right;' class='btn btn-primary btn-sm pt-2 pb-2'> Mover </button>";
-   }
-   function botaoRemover(){
-      return "<button type='button' name='remover' style='float: right;' class='btn btn-primary btn-sm pt-2 pb-2'> Remover </button>";
-   }
-
-   $(document).on('click', "button", function(){
-      if($(this).attr("name")=='mover'){
-         let nome = $(this).parent().clone().children().remove().end().text();
-         $("#listaDir").append($("<li class='list-group-item'>"+ nome +botaoRemover() + "</li>").hide().fadeIn(1000));
-         $(this).parent().fadeOut(1000, function(){
-            $(this).remove();
-         });
-         $(this).attr("disabled", true);
-      }
-      else if($(this).attr("name")=='remover'){
-         $(this).parent().fadeOut(1000, function(){
-            $(this).remove();
-         });
-         
-      }
-      else{
-      }
+$( function() {
+   $( "#slider-range" ).slider({
+     range: true,
+     min: 0,
+     max: 1000,
+     values: [ 0, 1000],
+     slide: function( event, ui ) {
+       $( "#amount" ).val( "R$" + ui.values[ 0 ] + " - R$" + ui.values[ 1 ] );
+     }
    });
-});
+   $( "#amount" ).val( "R$" + $( "#slider-range" ).slider( "values", 0 ) +
+     " - R$" + $( "#slider-range" ).slider( "values", 1 ) );
+ } );
